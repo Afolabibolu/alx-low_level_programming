6e5c43c2cbd613ce9e59597a4feaa3420a9ef37b
Afolabibolu <boluafolabidavid@gmail.com>
2022-10-13 17:28:14 -0700

    lovely

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+
+/**
+ * * main - main function
+ * *
+ * * Description: using the main function
+ * * Return: 0
+*/
+
+int main(void)
+
+{
+
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+
+}
