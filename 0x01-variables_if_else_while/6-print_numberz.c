dad236d2a4c178bce64c1c733741f1a1ef82d8db
Afolabibolu <boluafolabidavid@gmail.com>
2022-10-14 19:32:23 -0700

    lovely

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int num;
+for (num = '0'; num <= '9'; num++)
+{
+putchar(num);
+}
+putchar('\n');
+return (0);
+}
